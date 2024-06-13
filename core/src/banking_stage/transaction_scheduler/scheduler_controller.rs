558477065f9d71417899ed4bb92c34293d93a143
Andrew Fitzgerald <apfitzge@gmail.com>
2024-06-13 09:14:05 +0000

    optimize using drain

--- core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
+++ core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
@@ -426,14 +426,14 @@ impl SchedulerController {
 
         const CHUNK_SIZE: usize = 128;
         let lock_results: [_; CHUNK_SIZE] = core::array::from_fn(|_| Ok(()));
+
+        let mut arc_packets = Vec::with_capacity(CHUNK_SIZE);
+        let mut transactions = Vec::with_capacity(CHUNK_SIZE);
+        let mut fee_budget_limits_vec = Vec::with_capacity(CHUNK_SIZE);
+
         let mut error_counts = TransactionErrorMetrics::default();
         for chunk in packets.chunks(CHUNK_SIZE) {
             let mut post_sanitization_count: usize = 0;
-
-            let mut arc_packets = Vec::with_capacity(chunk.len());
-            let mut transactions = Vec::with_capacity(chunk.len());
-            let mut fee_budget_limits_vec = Vec::with_capacity(chunk.len());
-
             chunk
                 .iter()
                 .filter_map(|packet| {
@@ -470,9 +470,9 @@ impl SchedulerController {
 
             let mut post_transaction_check_count: usize = 0;
             for (((packet, transaction), fee_budget_limits), _) in arc_packets
-                .into_iter()
-                .zip(transactions)
-                .zip(fee_budget_limits_vec)
+                .drain(..)
+                .zip(transactions.drain(..))
+                .zip(fee_budget_limits_vec.drain(..))
                 .zip(check_results)
                 .filter(|(_, check_result)| check_result.0.is_ok())
             {
